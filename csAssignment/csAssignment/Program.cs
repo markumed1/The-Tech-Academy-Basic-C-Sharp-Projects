8480c72e9de28694831e6326bfa5cc703186d5d8
Mark Medrano <markumed1@hotmail.com>
2022-04-29 15:05:12 -1000

    CS Assignment
    Basic CS console

---
+++ csAssignment/csAssignment/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+    class Program
+    {
+        static void Main()
+        {
+        Console.WriteLine("Welcome to ACME Systems. Remember, we're \"accounting\" on you!");
+        Console.ReadLine();
+        }
+    }
+

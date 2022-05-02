69d7518c50e511c5f6b883f2af9b18652febcb30
Mark Medrano <markumed1@hotmail.com>
2022-05-02 12:52:02 -1000

    ConsoleAppAssignment
    Code

--- ConsoleAppAssignment/ConsoleAppAssignment/Program.cs
+++ ConsoleAppAssignment/ConsoleAppAssignment/Program.cs
@@ -13,14 +13,16 @@ namespace ConsoleAppAssignment
             //// Type a numeric number of your choice
             //Console.WriteLine("Enter a numeric number:");
 
-            //// Create a int variable that stores the number that the user enters
+            ///Create a int variable that stores the number that the user enters
             //int num1 = Convert.ToInt32(Console.ReadLine());
 
             //// Create a int varibale that is 50. That will multipy user's input and 50.
             //int num2 = 50;
 
-            //// follow PRODUCT guide line and you get your output
+            ///does a math operation with the number the user entered
             //int product = num1 * num2;
+
+            //dispalys the result
             //Console.WriteLine(product);
 
             //***************************************************************//
@@ -33,23 +35,28 @@ namespace ConsoleAppAssignment
             //int num1 = Convert.ToInt32(Console.ReadLine());
             //int num2 = 25;
 
-            //// follow TOTAL guide line and you get your output
+            ///does a math operation with the number the user entered
             //int product = num1 + num2;
+
+            //dispalys the result
             //Console.WriteLine(product);
 
             //*************************************************************//
 
 
-            //// Type a numeric number of your choice
+            //Type a numeric number of your choice
             Console.WriteLine("Enter a numeric number:");
 
-            //// Create a variable that stores the number that the user enters
-            decimal quotient = 12.5 / val2;
-            int result = Convert.ToInt32(quotient);
+            //Create a variable that stores the number that the user enters
+            decimal val2 = Convert.ToDecimal(Console.ReadLine());
+
+            //Does a math operation with the number the user entered
+            decimal quotient = val2 / 12.5m;
+
+            // dispalys the result
             Console.WriteLine(quotient);
 
-            
-            
+
 
             //*************************************************************//
 
@@ -62,7 +69,31 @@ namespace ConsoleAppAssignment
 
             //// using boolean for TRUE or False
             //bool trueOrFalse = num1 > 50;
+
+            //dispalys the result
             //Console.Write(trueOrFalse.ToString());
+
+            //***********************************************************//
+
+
+
+            //// Type a numeric number of your choice
+            //Console.WriteLine("Enter a numeric number:");
+
+            ////Create a variable that stores the number of that the user enters
+            //int num1 = Convert.ToInt32(Console.ReadLine());
+            //int num2 = 7;
+
+            ///does a math operation with the number the user entered
+            //int remainder = num1 % num2;
+
+            // dispalys the result
+            //Console.WriteLine(remainder);
+
+
+
+
+
 
 
 

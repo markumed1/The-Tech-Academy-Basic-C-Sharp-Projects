ea8bb71278d38192cdf2c63575a0c3c8cd075b82
Mark Medrano <markumed1@hotmail.com>
2022-05-03 10:26:20 -1000

    BooLeanLogic
    operators

---
+++ BooleanLogic/BooleanLogic/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BooleanLogic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            ////The AND operator
+            //Console.WriteLine(true && false);
+            //Console.WriteLine(true && true);
+            //Console.WriteLine(false && false);
+            //Console.WriteLine(true && true && true && false);
+            Console.WriteLine(true && true && true || false);
+
+
+            ////The OR operator
+            //Console.WriteLine(true || true);
+            //Console.WriteLine(true || false);
+            //Console.WriteLine(false || false);
+
+
+            ////The EQUAL operator
+            //Console.WriteLine(true == true);
+            //Console.WriteLine(true == false);
+            //Console.WriteLine(false == false);
+
+
+            ////The does NOT EQUAL operator
+            //Console.WriteLine(true != true);
+            //Console.WriteLine(true != false);
+            //Console.WriteLine(false != false);
+
+            ////The XOR operator 
+            //Console.WriteLine(true ^ true);
+            //Console.WriteLine(true ^ false);
+            //Console.WriteLine(false ^ false);
+
+            Console.ReadLine();
+        }
+    }
+}

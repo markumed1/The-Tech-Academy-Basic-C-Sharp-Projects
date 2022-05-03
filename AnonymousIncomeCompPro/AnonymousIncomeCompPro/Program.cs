d4f8e76f00b8691fe53fdd29afcce0a44751f877
Mark Medrano <markumed1@hotmail.com>
2022-05-02 20:22:39 -1000

    IncomeProgram
    Push code for review

---
+++ AnonymousIncomeCompPro/AnonymousIncomeCompPro/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnonymousIncomeCompPro
+{
+    class Program
+    {
+        public static int Annual_Salary { get; private set; }
+
+        static void Main(string[] args)
+        {
+            Double annual_salary;
+
+            //Starting the program with intro title
+            Console.WriteLine("Anonymous Income Comparison Program\n");
+
+            Console.WriteLine("Person 1");
+            Console.WriteLine("hourly Rate?");
+            string hourlyRate = Console.ReadLine();
+            Console.WriteLine("Your hourly rate is: " + hourlyRate + "\n");
+
+            Console.WriteLine("hours worked per week?");
+            string weeklyHours = Console.ReadLine();
+            Console.WriteLine("Your weekly hours is: " + weeklyHours + "\n");
+
+            Console.WriteLine("Annual salary");
+            Console.WriteLine("Person 1: ");
+            annual_salary = Convert.ToInt32(Console.ReadLine());
+
+            
+
+            Console.ReadLine();
+          
+            
+        }
+
+    }
+}

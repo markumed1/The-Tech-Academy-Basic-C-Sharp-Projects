0c2db41c0023d12a8a354ecbe6ea78de600ce2fd
Mark Medrano <markumed1@hotmail.com>
2022-05-03 08:01:04 -1000

    Assignment
    Updated code for review

--- AnonymousIncomeCompPro/AnonymousIncomeCompPro/Program.cs
+++ AnonymousIncomeCompPro/AnonymousIncomeCompPro/Program.cs
@@ -18,22 +18,42 @@ namespace AnonymousIncomeCompPro
 
             Console.WriteLine("Person 1");
             Console.WriteLine("hourly Rate?");
-            string hourlyRate = Console.ReadLine();
-            Console.WriteLine("Your hourly rate is: " + hourlyRate + "\n");
+            double hourlyRate1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Your hourly rate is: " + hourlyRate1 + "\n");
 
             Console.WriteLine("hours worked per week?");
-            string weeklyHours = Console.ReadLine();
-            Console.WriteLine("Your weekly hours is: " + weeklyHours + "\n");
-
-            Console.WriteLine("Annual salary");
-            Console.WriteLine("Person 1: ");
-            annual_salary = Convert.ToInt32(Console.ReadLine());
-
+            double weeklyHours1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Your weekly hours are: " + weeklyHours1 + "\n");
             
+
+            Console.WriteLine("Person 2");
+            Console.WriteLine("hourly Rate?");
+            double hourlyRate2 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Your hourly rate are: " + hourlyRate2 + "\n");
+
+            Console.WriteLine("hours worked per week?");
+            double weeklyHours2 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Your weekly hours are: " + weeklyHours2 + "\n");
+
+            Console.WriteLine("Annual salary of Person 1");
+            Console.WriteLine("Person 1: ");
+            double annual_salary1 = hourlyRate1 * weeklyHours1 * 52;
+            Console.WriteLine(annual_salary1);
+
+            Console.WriteLine("Annual salary of Person 2");
+            Console.WriteLine("Person 2: ");
+            double annual_salary2 = hourlyRate2 * weeklyHours2 * 52;
+            Console.WriteLine(annual_salary2);
+            Console.WriteLine("Does Person 1 make more money than Person 2?");
+            Console.WriteLine(annual_salary1 > annual_salary2);
+
 
             Console.ReadLine();
-          
-            
+
+
+
+
+
         }
 
     }

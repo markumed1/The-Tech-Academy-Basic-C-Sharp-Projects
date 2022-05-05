af2c77ef519ee25cb0c7c9efa08989c3871a3c92
Mark Medrano <markumed1@hotmail.com>
2022-05-05 09:09:02 -1000

    Update
    Update branch

--- Branching/Branching/Program.cs
+++ Branching/Branching/Program.cs
@@ -10,6 +10,63 @@ namespace Branching
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("What is your favorite number?");
+            int favNum = Convert.ToInt32(Console.ReadLine());
+
+            string result = favNum == 12 ? "You have an awesome favorite number." : "You do not have an awesome favorite number. ";
+
+            Console.WriteLine(result);
+            Console.ReadLine();
+
+            //int roomTemperature = 70;
+
+            //Console.WriteLine("Hi, what is your name?");
+            //string name = Console.ReadLine();
+
+            //Console.WriteLine("Hi, " + name + ", what is the temperature where you are?");
+            //int currentTemperature = Convert.ToInt32(Console.ReadLine());
+
+            //if (currentTemperature == roomTemperature)
+            //{
+            //    Console.WriteLine("It is exactly room temperature.");
+            //}
+            //else if (currentTemperature > roomTemperature)
+            //{
+            //    Console.WriteLine("It is warmer than room temperature.");
+            //}
+            //else if (roomTemperature > currentTemperature)
+            //{
+            //    Console.WriteLine("It is colder than room temperature.");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("uhhhh....something went wrong.");
+            //}
+
+            //int currentTemperature = 80;
+            //int roomTemperature = 70;
+
+            //string comparisonResult = currentTemperature == roomTemperature ? "It is room temp." : "It is not room temp.";
+
+            //Console.WriteLine(comparisonResult);
+            //Console.ReadLine();
+            //if (currentTemperature == roomTemperature)
+            //{
+            //    Console.WriteLine("it is exactly room temprature.");
+            //}
+            //else if (currentTemperature > roomTemperature)
+            //{
+            //    Console.WriteLine("It is warmer than room temprature.");
+            //}
+            //else if (roomTemperature > currentTemperature)
+            //{
+            //    Console.WriteLine("Room temperature is warmer than current temprature");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("It is not exaclty room temprature.");
+            //}
+            Console.ReadLine();
         }
     }
 }
---
+++ BranchingSubmissionAssignment/BranchingSubmissionAssignment/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BranchingSubmissionAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Opening program with welcome message.
+            Console.WriteLine("Welcome to Package Express. Please follow the instructions below:\n");
+            int maxPackageWeight = 50;
+
+            Console.WriteLine("Please enter the package weight.");
+
+            //User enters current weight. 'Convert.ToInt32' method converts the string representation of a
+            //number in a specified base to an equivalent 32-bit signed integer.
+            int currentPackageWeight = Convert.ToInt32(Console.ReadLine());
+
+            if (currentPackageWeight > maxPackageWeight)
+            {
+                Console.WriteLine("Package too heavy to be shipped via Package Express. Have a good day."); //this message will be displayed onto console if weight is > max weight.
+            }
+
+            int maxPackageWidth = 50;
+
+            Console.WriteLine("Please enter package width.");
+            int currentPackageWidth = Convert.ToInt32(Console.ReadLine());
+
+
+            int maxPackageHeight = 50;
+
+            Console.WriteLine("Please enter package width.");
+            int currentPackageHeight = Convert.ToInt32(Console.ReadLine());
+
+
+            int maxPackageLength = 50;
+
+            Console.WriteLine("Please enter package width.");
+            int currentPackageLength = Convert.ToInt32(Console.ReadLine());
+
+            if (currentPackageWidth > maxPackageWidth && currentPackageHeight > maxPackageHeight && currentPackageLength > maxPackageLength)
+            {
+                Console.WriteLine("Package too big to be shipped via Package Express.");
+            }
+
+
+            //if (currentPackageWidth > maxPackageWidth)
+            //{
+                
+            //}
+            //else if (currentPackageWidth > maxPackageHeight)
+            //{
+
+            //}
+            //else if (currentPackageWidth > maxPackageLength)
+
+
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+}

595aabecf922c681ebd68482bd729f6809c3ce61
Mark Medrano <markumed1@hotmail.com>
2022-05-05 10:23:05 -1000

    BranchingAssignment
    Pushing code for review

--- BranchingSubmissionAssignment/BranchingSubmissionAssignment/Program.cs
+++ BranchingSubmissionAssignment/BranchingSubmissionAssignment/Program.cs
@@ -12,7 +12,7 @@ namespace BranchingSubmissionAssignment
         {
             //Opening program with welcome message.
             Console.WriteLine("Welcome to Package Express. Please follow the instructions below:\n");
-            int maxPackageWeight = 50;
+            
 
             Console.WriteLine("Please enter the package weight.");
 
@@ -20,45 +20,44 @@ namespace BranchingSubmissionAssignment
             //number in a specified base to an equivalent 32-bit signed integer.
             int currentPackageWeight = Convert.ToInt32(Console.ReadLine());
 
-            if (currentPackageWeight > maxPackageWeight)
+            if (currentPackageWeight > 50)
             {
                 Console.WriteLine("Package too heavy to be shipped via Package Express. Have a good day."); //this message will be displayed onto console if weight is > max weight.
             }
-
-            int maxPackageWidth = 50;
-
-            Console.WriteLine("Please enter package width.");
-            int currentPackageWidth = Convert.ToInt32(Console.ReadLine());
-
-
-            int maxPackageHeight = 50;
-
-            Console.WriteLine("Please enter package width.");
-            int currentPackageHeight = Convert.ToInt32(Console.ReadLine());
-
-
-            int maxPackageLength = 50;
-
-            Console.WriteLine("Please enter package width.");
-            int currentPackageLength = Convert.ToInt32(Console.ReadLine());
-
-            if (currentPackageWidth > maxPackageWidth && currentPackageHeight > maxPackageHeight && currentPackageLength > maxPackageLength)
+            else
             {
-                Console.WriteLine("Package too big to be shipped via Package Express.");
+                Console.WriteLine("Please enter package width.");
+                int currentPackageWidth = Convert.ToInt32(Console.ReadLine());
+
+
+
+
+                Console.WriteLine("Please enter package width.");
+                int currentPackageHeight = Convert.ToInt32(Console.ReadLine());
+
+
+
+
+                Console.WriteLine("Please enter package width.");
+                int currentPackageLength = Convert.ToInt32(Console.ReadLine());
+
+                if (currentPackageWidth + currentPackageHeight + currentPackageLength > 50)
+                {
+                    Console.WriteLine("Package too big to be shipped via Package Express.");
+                }
+                else 
+                {
+                    decimal quote = currentPackageWidth * currentPackageHeight * currentPackageLength * currentPackageWeight / 100;
+                    Console.WriteLine("Your estimated total for shipping this package is: " + "$" + quote + " Thank you!");
+                }
             }
-
-
-            //if (currentPackageWidth > maxPackageWidth)
-            //{
                 
-            //}
-            //else if (currentPackageWidth > maxPackageHeight)
-            //{
-
-            //}
-            //else if (currentPackageWidth > maxPackageLength)
+     
 
 
+
+
+           
 
 
 

d4c14f619055500ea9b11d53c6f73818b664d378
Mark Medrano <markumed1@hotmail.com>
2022-05-06 08:58:21 -1000

    ComparisonAssignment
    Pushing code for review

---
+++ BooleanComparisonAssignment/BooleanComparisonAssignment/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BooleanComparisonAssignment
+{
+    class Program
+    {
+        public static string Red { get; private set; }
+
+        static void Main(string[] args)
+        {
+            //Opening program with question to console
+            Console.WriteLine("Guess my favourite number between 1 and 50?");
+            int number = Convert.ToInt32(Console.ReadLine()); //declaring a variable. using convert method Converts the specified string representation of a number.
+            bool isGuessed = number == 47; //my favourite number
+
+            // The do while loop stops execution exits when a boolean condition evaluates to false
+            do
+            {
+                // Switch statement is a multiway branch statement. It provides an efficient way to
+                // transfer the execution to different parts of a code based on the value of the expression
+                switch (number)
+                {
+                    case 33: //switch case statement is a selection statement. In other words is 33 the correct number.
+                        Console.WriteLine("You guessed 33. Try again.");
+                        Console.WriteLine("Guess my favourite #?");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break; //the break statement is used to terminate a loop(for, if, while, etc.)
+                    case 50:
+                        Console.WriteLine("You guessed 50. Try again.");
+                        Console.WriteLine("Guess my favourite #?");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case 47:
+                        Console.WriteLine("You guessed 47. That is correct");
+                        isGuessed = true;
+                        break;
+                    case 10:
+                        Console.WriteLine("You guessed 10. Try again.");
+                        Console.WriteLine("Guess my favourite #?");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    default:
+                        Console.WriteLine("You  are wrong.");
+                        Console.WriteLine("Guess my favourite #?");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+
+
+                }
+
+            }
+            while (!isGuessed);
+
+            Console.ReadLine();
+
+        }
+        
+
+
+
+            
+    }   
+}

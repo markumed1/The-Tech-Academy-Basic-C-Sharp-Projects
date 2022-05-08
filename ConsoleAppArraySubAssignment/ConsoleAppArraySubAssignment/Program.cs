351406c4c128f919d9d922da3147a4603b70a860
Mark Medrano <markumed1@hotmail.com>
2022-05-08 13:34:54 -1000

    List and Arrays
    Code Review

--- ConsoleAppArraySubAssignment/ConsoleAppArraySubAssignment/Program.cs
+++ ConsoleAppArraySubAssignment/ConsoleAppArraySubAssignment/Program.cs
@@ -18,17 +18,61 @@ namespace ConsoleAppArraySubAssignment
             //Console.WriteLine("You chose car brand: " + JapanCars[userIndex]); //This line will display the car brand at that index. 
             ////Number stored in the "userIndex" variable.
 
+
             //Console.ReadLine();
 
 
-            //one-dimensional array of INTEGERS
-            int[] myNumbers = { 10, 5, 15, 20, 30 };
-            Console.WriteLine("User please enter a number from 0 to 4\n");
+            ////one - dimensional array of integers
+            //int[] mynumbers = { 10, 5, 15, 20, 30 };
+            //Console.WriteLine("user please enter a number from 0 to 4\n");
 
-            int userIndex = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("You chose number: " + myNumbers[userIndex]);
+            //int userindex = Convert.ToInt32(Console.ReadLine());
+
+            //if (userindex <= 4) //If statement, user enters a number out of range
+            //{
+            //    Console.WriteLine("index does not exist " + mynumbers[userindex]); //Displays message
+            //}
+            //else
+            //{
+            //    Console.WriteLine($"{userindex} is out of range ");
+            //}
+
+
+            //Console.ReadLine();
+
+
+
+            //Create a list of strings.
+            string[] str = new string[] { "Howzit", "braddah", "Mark", "shoots!" };
+
+            int strChoice;
+
+            List<string> wordList = new List<string>(); //Storing strings in collection
+            wordList.Add("Rajah");
+            wordList.Add("Sup Bulleh");
+            wordList.Add("Ho ya");
+            wordList.Add("Oh nah");
+
+
+            Console.WriteLine("User please select item between 0 - 3"); //asking user to choose a number
+            strChoice = Convert.ToInt32(Console.ReadLine());
+
+            //if elese statment if user chooses number beyong range
+            if (strChoice < str.Length)
+            {
+                Console.WriteLine(str[Convert.ToInt32(strChoice)]);
+            }
+            else
+            {
+                Console.WriteLine("That number does not exist in the index.");
+            }
+
+
+
 
             Console.ReadLine();
+
+
 
         }
     }

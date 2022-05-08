c86f9942388be5ebe3fc4ec8bf62f733689ea246
Mark Medrano <markumed1@hotmail.com>
2022-05-08 10:02:37 -1000

    Assignments and Exercises

---
+++ ArraysAndLists/ArraysAndLists/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+    class Program
+    {
+        static void Main()
+        {
+        //Lists
+        //List<int> intList = new List<int>();
+        //intList.Add(4);
+        //intList.Add(10);
+        //intList.Remove(10);
+
+
+        List<string> intList = new List<string>();
+        intList.Add("Hello");
+        intList.Add("Mark");
+        intList.Remove("Mark");
+
+        Console.WriteLine(intList[0]);
+        Console.ReadLine();
+
+              byte[] byteArray = 
+        //    //Array fundementals
+        //    int[] numArray = new int[5];
+        //    numArray[0] = 5;
+        //    numArray[1] = 2;
+        //    numArray[2] = 10;
+        //    numArray[3] = 200;
+        //    numArray[4] = 5000;
+
+        //int[] numArray1 = new int[] { 5, 2, 10, 200, 5000, 600, 2300 }; //this is the same thing as from lines 9 to 14.
+
+        //int[] numArray2 = { 5, 2, 10, 200, 5000, 600, 2300 }; // this is also the same and simplier way.
+
+        //numArray2[5] = 650;
+
+        //Console.WriteLine(numArray2[5]);
+        //Console.ReadLine();
+
+
+        }
+    }
+
---
+++ ConsoleAppArraySubAssignment/ConsoleAppArraySubAssignment/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppArraySubAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ////one-dimensional array of STRINGS
+            //string[] JapanCars = { "Honda", "Toyota", "Nissan", "Subaru", "Mitsubishi" };
+            //Console.WriteLine("User please enter a number from 0 to 4\n");
+
+            //int userIndex = Convert.ToInt32(Console.ReadLine()); //User select index
+            //Console.WriteLine("You chose car brand: " + JapanCars[userIndex]); //This line will display the car brand at that index. 
+            ////Number stored in the "userIndex" variable.
+
+            //Console.ReadLine();
+
+
+            //one-dimensional array of INTEGERS
+            int[] myNumbers = { 10, 5, 15, 20, 30 };
+            Console.WriteLine("User please enter a number from 0 to 4\n");
+
+            int userIndex = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("You chose number: " + myNumbers[userIndex]);
+
+            Console.ReadLine();
+
+        }
+    }
+}

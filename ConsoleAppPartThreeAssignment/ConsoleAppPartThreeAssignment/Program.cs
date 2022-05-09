a98f78c9e774fbec4366697f64357fdd5e34b758
Mark Medrano <markumed1@hotmail.com>
2022-05-09 12:53:38 -1000

    Assignments
    Part three

---
+++ ConsoleAppPartThreeAssignment/ConsoleAppPartThreeAssignment/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppPartThreeAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string[] items = { "10", "30", "80", "6" };
+
+            //for (int i = 0; i < items.Length; i++)
+            //{
+            //    Console.WriteLine(items[i]);
+            //}
+            //Console.ReadLine();
+
+
+
+            string[] items = { "10", "30", "80", "6" };
+
+            for (int i = 0; i <= items.Length; i++)
+            {
+                Console.WriteLine(items[i]);
+            }
+            Console.ReadLine();
+        }
+    }
+}

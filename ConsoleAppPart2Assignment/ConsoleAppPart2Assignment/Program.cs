947d9e13671a93d48f3459ad4c23ac1ce0ee8440
Mark Medrano <markumed1@hotmail.com>
2022-05-09 11:27:11 -1000

    Infinite loop
    Infinite loops and breaking them

---
+++ ConsoleAppPart2Assignment/ConsoleAppPart2Assignment/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppPart2Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ////Creating infinite loop. Using while loop
+            //while (true)
+            //{
+            //    //This statement will be printed infinite times
+            //    Console.WriteLine("Boto Boyz for life!");
+
+
+
+            //Breaking Infinite loop
+            int x = 1;
+
+            while (true)
+            {
+                if (x == 7)
+                    break;
+
+                Console.WriteLine("Boto Boyz for life!");
+                x++;
+
+                Console.ReadLine();
+            }
+            
+        }
+    }
+}

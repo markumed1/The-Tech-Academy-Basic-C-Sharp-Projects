4fcb7e293400d895349691dc68a14eae40eab704
Mark Medrano <markumed1@hotmail.com>
2022-05-08 20:39:00 -1000

    Console App Part One Assignment
    Array, string and loops

---
+++ ConsoleAppPart1Assignment/ConsoleAppPart1Assignment/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppPart1Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Declaring single dimensional array
+            string[] = Books = new string[5];
+            Books[0] = "The Art of Racing in The Rain";
+            Books[1] = "My Dog Tulip";
+            Books[2] = "A Big Little Life";
+            Books[3] = "Lulu Walks the Dog";
+            Books[4] = "White Fang";
+            Books[5] = "The Phantom Tollbooth";
+        }
+    }
+}

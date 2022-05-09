0a4f732b9cae0002e84258b2250390a6e8d187d9
Mark Medrano <markumed1@hotmail.com>
2022-05-09 10:58:01 -1000

    Code update
    Update

--- ConsoleAppPart1Assignment/ConsoleAppPart1Assignment/Program.cs
+++ ConsoleAppPart1Assignment/ConsoleAppPart1Assignment/Program.cs
@@ -13,12 +13,19 @@ namespace ConsoleAppPart1Assignment
             //Declaring single dimensional array
             string[] names = { "Keven", "Mark", "Miles", "Katrina" };
 
+            // prompts user to input text
             Console.WriteLine("User please input some text.");
+            string sSize = Console.ReadLine();
 
-            //for(int m = 0; m < names.Length; m++)\
 
+            //A for loop that iterates through each string in the array and adds the user's
+            //text input to the end of each string.
+            for (int i = 0; i < names.Length; i++) 
+            {
+                Console.WriteLine(names[i] + sSize);
+                
+            }
             Console.ReadLine();
-         
-        }
+        }       
     }   
 }

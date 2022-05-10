78d99c7de0510ba8c1272a0a370e715c87e35da0
Mark Medrano <markumed1@hotmail.com>
2022-05-10 10:58:12 -1000

    Loop assignments
    Loop, if, else, else if...pushing code for review

--- ConsoleAppPartSixAssignment/ConsoleAppPartSixAssignment/Program.cs
+++ ConsoleAppPartSixAssignment/ConsoleAppPartSixAssignment/Program.cs
@@ -10,7 +10,31 @@ namespace ConsoleAppPartSixAssignment
     {
         static void Main(string[] args)
         {
-           
+           List<string> names = new List<string>(); //list of strings
+            names.Add("Kurt Cobain");
+            names.Add("Talib Kweli");
+            names.Add("Bruno Mars");
+            names.Add("Eminem");
+            names.Add("Rihanna");
+            names.Add("Kurt Cobain");
+
+            List<string> newNames = new List<string>(); //Empty list
+
+            //For each loop. Loop through the collection and each time through
+            //loop it will use the next name from the collection
+            foreach (string name in names)
+            {
+                if (newNames.Contains(name)) // using if statement if the condition is true (name exist on list)
+                {
+                    Console.WriteLine(name + " is already on the list");
+                }
+            else if(!newNames.Contains(name)) //using else if condition is not true (name not on list)
+                {
+                    newNames.Add(name);
+                    Console.WriteLine(name + " has not appeared on the list");
+                }
+            }
+            Console.ReadLine();
         }
     }
 }

cfb73d54dc5e6dfb251d4006e2ec154ce2321fa1
Mark Medrano <markumed1@hotmail.com>
2022-05-10 13:20:24 -1000

    Strings and Integers
    push for code review

---
+++ StringsAndIntegersAssignment/StringsAndIntegersAssignment/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringsAndIntegersAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Create list of ints.
+            List<int> numberList = new List<int>() { 2, 4, 6, 8, 10, 12, 14, 16, 18, 20 };
+
+            Console.WriteLine("user pick a number");
+            int userNumber = Convert.ToInt32(Console.ReadLine());
+            string userInput = Console.ReadLine();
+
+            if (userInput == "0")
+            {
+                for (int i = 0; i < numberList.Count; i++)
+                {
+                    Console.WriteLine(numberList[i]);
+                    Console.WriteLine(" , ");
+                }
+            }
+        
+
+        }
+        
+        
+    }
+}

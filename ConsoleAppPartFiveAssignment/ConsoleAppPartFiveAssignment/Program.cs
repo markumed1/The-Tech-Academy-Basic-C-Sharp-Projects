5945c7a2d975c9c1d6d44dea8aed864bb8a91677
Mark Medrano <markumed1@hotmail.com>
2022-05-09 18:11:56 -1000

    Assignments
    Code review

--- ConsoleAppPartFiveAssignment/ConsoleAppPartFiveAssignment/Program.cs
+++ ConsoleAppPartFiveAssignment/ConsoleAppPartFiveAssignment/Program.cs
@@ -10,6 +10,24 @@ namespace ConsoleAppPartFiveAssignment
     {
         static void Main(string[] args)
         {
+            List<int> list = new List<int>() { 3, 5, 2, 7, 7, 5, 6 };
+
+            Console.WriteLine("User, please select text to search in the list");
+            string userSelect = Console.ReadLine();
+
+            if (true)
+            {
+                for (int i = 0; i < list.Count; i++)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Your selection is not in the list");
+            }
+
+            Console.ReadLine();
         }
     }
 }

ab45eabf52cd184330c8431e668a0ffaa5bbeca4
Mark Medrano <markumed1@hotmail.com>
2022-05-12 21:22:12 -1000

    MethodSubmission
    Assignment

---
+++ MethodSubmissionAssignment/MethodSubmissionAssignment/Method.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodSubmissionAssignment
+{
+    class Method
+    {
+        public int MethodMath(int num1, int num2 = 5) //Created method 'MethodMath, two integers as pramaters in '()'.
+        {
+            
+            {
+                return num1 + num2;
+            }
+        }
+    }
+}
---
+++ MethodSubmissionAssignment/MethodSubmissionAssignment/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodSubmissionAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Method MathOperator = new Method();
+
+            Console.WriteLine("User please input two numbers, one at a time. Don't need to enter a second number, Just press enter.");
+
+            int num1 = 6;
+            int num2 = 5;
+
+            
+            Console.WriteLine(MathOperator.MethodMath(num1));
+            Console.WriteLine(MathOperator.MethodMath(num2));
+
+
+            Console.ReadLine();
+        }
+    }
+}

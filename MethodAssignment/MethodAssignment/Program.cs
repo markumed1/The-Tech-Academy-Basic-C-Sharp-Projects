1fc7ca9daf7cd601aa273d52de4dee21bca0da9e
Mark Medrano <markumed1@hotmail.com>
2022-05-13 14:04:59 -1000

    Assignment

---
+++ MethodAssignment/MethodAssignment/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program myProgram = new Program(); //instantiate
+            myProgram.mathMethod(2, 3);
+            myProgram.mathMethod(num: 10, num2: 5);
+           
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ MethodAssignment/MethodAssignment/mathMethod.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodAssignment
+{
+        class MethodClass
+        {
+            //Create void method
+            public void math(int num1, int num2)
+            {
+                int result;
+                int result2;
+
+                result = num1;
+                result2 = num2 + 2;
+                Console.WriteLine(result2);
+            }
+        }
+}
